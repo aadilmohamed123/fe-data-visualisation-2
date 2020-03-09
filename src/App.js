cdb9bd47d4b7054aea96509865670168d95f87fb
aadilmohamed123 <aadil.mohamed@hotmail.co.uk>
2020-03-09 16:39:40 +0000

    trying to spread, end of Monday

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import LocationForm from "./components/LocationForm";
 import React, { Component } from "react";
+import WeatherCard from "./components/WeatherCard";
 
 class App extends Component {
+  state = { weatherInfo: "" };
   render() {
     return (
       <>
@@ -11,6 +13,7 @@ class App extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
+        <WeatherCard {...this.state.weatherInfo} />
       </>
     );
   }
@@ -18,7 +21,11 @@ class App extends Component {
     console.log(e.target.value);
     this.setState({ newCity: e.target.value });
   };
-  handleSubmit = () => {
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  componentDidMount() {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=London&&appid=44a29f02dde67ea362372acd6d5bc7d8`
     )
@@ -26,9 +33,9 @@ class App extends Component {
         return response.json();
       })
       .then(data => {
-        this.setState({ weatherInfo: data.weather });
+        this.setState({ weatherInfo: data });
       });
-  };
+  }
 }
 
 export default App;
